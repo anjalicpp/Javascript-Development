8757f9f9b0fb25a0a89042b17057b298e536f440
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-07-27 19:27:18 +0530

    Update 10_this_keyword.js

--- 01_basic/10_this_keyword.js
+++ 01_basic/10_this_keyword.js
@@ -1,1 +1,14 @@
+*****************************THIS KEYWORD*************************
+
+const user={
+    name:"Sham",
+    price:"99"
+
+    function display()
+{
+    //This refer to current context
+    console.log('${this.username} welcome!!!!');//Sham welcome!!!!
+    console.log(this);
+}
+}
 
