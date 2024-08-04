b55b045db569ec1e7dbfc6357144dafe470ea2a8
Anjali <91792558+anjalicpp@users.noreply.github.com>
2024-08-04 11:00:53 +0530

    Update 13_filtermap and reduce.js

--- 01_basic/13_filtermap and reduce.js
+++ 01_basic/13_filtermap and reduce.js
@@ -26,5 +26,70 @@ const newNum = myNum.map((num)=>num+10)
                     .filter((num)=>num>=40)
 console.log(newNums);//[41,51,61,71,81,91,10]
 
+**********************************************
+REDUCE FUNCTION:
+  
+  const myNums = [1,2,3]
+const myTotal = myNums.reduce(function(acc,curval){
+  console.log('acc:${acc} and currval:${currval}');
+  return acc+currval},0);//this zero is initial value passed to accumulator 
+}
+//we can set any initial value to pass to accumulator.
+This parameter is important for accumulator as initially it does not know initial value.
+  The initial value is first value accumulator takes after that it will take vale according to the function defined.
+
+  const myToatl = myNums.reduce(function(acc,currval))
+{
+  console.log('acc:${acc} and currval :${currval});
+              return acc + currval},0)
+console.log(myTotal);
+//OP: acc:0 and currval:1
+      acc:1 and currval:2
+      acc:3 and currval:3
+      6
+
+Same function can be written with the help of arrow function:
+  const myTotal = myNums.reduce((acc,curr)=>acc+curr,0)
+
+so in short basic structure of the reduce is:
+const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue,
+);
+
+console.log(sumWithInitial);
+// Expected output: 10
+
+//another example:
+
+const shoppingCart = [
+  {
+    itemName: "js course",
+    price:2999
+  },
+   {
+    itemName: "py course",
+    price:999
+  },
+   {
+    itemName: "mobile dev course",
+    price:5999
+  },
+   {
+    itemName: "data science course",
+    price:2999
+  },
+  
+]
+const PriceToPay = shoppingCart.reduce((acc,item)=>acc+item.price,0)
+console.log(PriceToPay);
+
+
+  
+
 
 
